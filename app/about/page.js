8949b9722fb1d313afa26e2cafb9fa433fa09eb8
MuhammadJunaid506 <93211293+MuhammadJunaid506@users.noreply.github.com>
2024-07-16 21:43:11 +0500

    about us

--- app/about/page.js
+++ app/about/page.js
@@ -4,6 +4,9 @@ import CustomBanner from '../components/CustomBanner'
 import Footer from '../components/Footer'
 
 const About = () => {
+  const bannerImages = [
+    image1, image2, image3
+  ];
   return (
     <div>
         <Navbar />
